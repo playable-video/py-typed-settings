6fde076cc4760b6d908c489870618133562f3fee
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-09-19 19:20:04 -0400

    [{py_typed_settings/,__init__.py,_data/logging.yml}] Remove logging.yml and handler code

--- py_typed_settings/__init__.py
+++ py_typed_settings/__init__.py
@@ -1,22 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
-from logging.config import dictConfig as _dictConfig
-from os import path
-
-import yaml
+from logging import getLogger as get_logger
 
 __author__ = "Samuel Marks"
 __version__ = "0.0.2"
 __description__ = "Generate typed settings.py from settings.yaml in Python 2, 3"
-
-
-def get_logger(name=None):
-    with open(path.join(path.dirname(__file__), "_data", "logging.yml"), "rt") as f:
-        data = yaml.load(f, Loader=yaml.SafeLoader)
-    _dictConfig(data)
-    return logging.getLogger(name=name)
-
 
 root_logger = get_logger()
